b4f7bc079b97c37e9ab389da003a76684c2c3a12
SixTreez <drew.molloy@live.com>
2018-04-20 12:32:56 -0700

    Add files via upload

---
+++ CardMaker/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CardMaker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult MakeCard()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult MakeCard(CardMaker.Models.CardBusiness cardbiz)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("MyCard", cardbiz);
+            }
+            else {
+                return View();
+            }
+        }
+    }
+}
