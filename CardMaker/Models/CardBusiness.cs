3002ebbf08f896056e66c615e4bcc117d3717b6b
SixTreez <drew.molloy@live.com>
2018-04-20 12:34:10 -0700

    Add files via upload

---
+++ CardMaker/Models/CardBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CardMaker.Models
+{
+    public class CardBusiness
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Sender { get; set; }
+        [Required(ErrorMessage = "Please enter a recipient")]
+        public string Recipient { get; set; }
+        [Required(ErrorMessage = "Please enter a message")]
+        public string Message { get; set; }
+    }
+
+}
